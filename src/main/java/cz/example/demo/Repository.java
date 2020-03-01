7ebd5e00eb8484eb013987601772767bed2ca9c8
Adderin <adderin@gmail.com>
2020-03-01 23:16:17 +0100

    initializing

---
+++ src/main/java/cz/example/demo/Repository.java
@@ -0,0 +1,25 @@
+package cz.example.demo;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Repository {
+	
+	static Map<String, Person> persons = new Hashtable<>();
+	
+	static {
+		persons.put("Andrew", new Person("Andrew", 31));
+		persons.put("John", new Person("John", 35));
+		persons.put("Petr", new Person("Petr", 38));
+	}
+	
+	
+	public Person personByName(String name) {
+		return persons.get(name);
+	}
+	
+	public Person addPerson(Person p) {
+		persons.put(p.getName(),p);
+		return p;
+	}
+}
