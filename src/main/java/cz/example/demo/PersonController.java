75ca8c371a046fac2a080636a2453fd6f737d7d2
Adderin <adderin@gmail.com>
2020-03-05 23:11:49 +0100

    adding POST and GET

--- src/main/java/cz/example/demo/PersonController.java
+++ src/main/java/cz/example/demo/PersonController.java
@@ -1,6 +1,8 @@
 package cz.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,25 @@ public class PersonController {
 	
 	Repository rep = new Repository();
 	
-	@RequestMapping("/person")
-	public Person person(@RequestParam(value = "name", required=true) String name) {
+	//@RequestMapping("/person")
+	@GetMapping("/person")
+	public Person person(
+			@RequestParam(value = "name", required=true) String name) {
 		return rep.personByName(name);
+	}
+	
+	@PostMapping("/person")
+	public Person personUpdate(
+			@RequestParam(value = "name", required=true) String name, 
+			@RequestParam(value = "age", required=true) int age) {
+		
+		Person p = rep.personByName(name);
+		if(p != null) {
+			p.setAge(age);
+			return p;
+		}
+		else 
+			return rep.addPerson(new Person(name, age));
 	}
 
 }
