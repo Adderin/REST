1db84b13ba79a9a8f323a4f071a175bfd55a4869
Adderin <adderin@gmail.com>
2020-03-05 23:16:17 +0100

    added person

--- src/main/java/cz/example/demo/Repository.java
+++ src/main/java/cz/example/demo/Repository.java
@@ -11,6 +11,7 @@ public class Repository {
 		persons.put("Andrew", new Person("Andrew", 31));
 		persons.put("John", new Person("John", 35));
 		persons.put("Petr", new Person("Petr", 38));
+		persons.put("Bob", new Person("Bob", 40));
 	}
 	
 	
